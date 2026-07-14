4a20daa3cbfc3801f074ab88510d00fde49b1dbb
Zuzana Benkova <zuzanka.dvorska@gmail.com>
2026-07-14 22:21:05 +0200

    novy commit

--- main.py
+++ main.py
@@ -3,7 +3,32 @@ aircraft = {
     "altitude": 10500,
     "speed": 850,
     "engine_temp": 91,
-    "fuel": 62
+    "fuel": 22
 }
 
 print(f"Flight: {aircraft['flight']}")
+
+def check_fuel(X):
+    if X >= 30:
+        return "OK"
+    else:
+        return "LOW FUEL"
+    
+status = check_fuel(aircraft["fuel"])
+print(status)
+
+if status == "LOW FUEL":
+    print("Pilot must land immediately!")
+
+
+def check_engine_temperature(eng_temp):
+    if eng_temp >= 100:
+        return "ENGINE OVERHEAT"
+    else:
+        return "ENGINE OK"
+
+status = check_engine_temperature(aircraft["engine_temp"])
+print(status)
+
+if status == "ENGINE OVERHEAT":
+    print("Pilot must land immediately!")
