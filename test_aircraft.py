483524fdeaa8e924890cd65350cc4aead12848cd
Zuzana Benkova <zuzanka.dvorska@gmail.com>
2026-07-20 11:38:07 +0200

    opravene testy

--- test_aircraft.py
+++ test_aircraft.py
@@ -6,21 +6,25 @@ def test_fuel_ok():
     assert result == ('FUEL OK', 'ENGINE OK')
 
     
-def test_fuel_ok():
+def test_low_fuel_engine():
     result = generate_aircraft_report(20, 90)
 
     assert result == ('LOW FUEL', 'ENGINE OK')
 
     
-def test_fuel_ok():
+def test_engine_overheat():
     result = generate_aircraft_report(60, 101)
 
     assert result == ('FUEL OK', 'ENGINE OVERHEAT')
 
-def test_low_fuel():
-    assert calculate_fuel_warning(20) == "LOW FUEL"
 
+def test_fuel_ok():
+    result = calculate_fuel_warning(20)
 
-#def test_calculate_fuel_warning():
-#    assert calculate_fuel_warning(30) == "CRITICALLY LOW FUEL, LAND IMMEDIATELY"
+    assert result == "FUEL OK"
+    
+def test_critical_low_fuel():
+    result = calculate_fuel_warning(19)
+
+    assert result == "CRITICALLY LOW FUEL, LAND IMMEDIATELY"
     
